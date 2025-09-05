d7a7e2c6451f2586b5b7bdc1b8dd0abaf3c56e74
Hadeel <Hadeel.Salman.al@gmail.com>
2025-09-06 00:27:15 +0300

    Initial Commit

---
+++ Circle/CircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleApp.swift
+//  Circle
+//
+//  Created by Hadeel on 06/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct CircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
